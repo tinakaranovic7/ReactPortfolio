4763de94722ccd461be8cdfe088a8e5bd4d4a038
Tina Karanovic <tinakaranovic7@gmail.com>
2020-04-07 23:51:08 -0400

    final version, portfolio updated

---
+++ README.md
@@ -0,0 +1,11 @@
+Portfolio template for developer/designers built with React. Use it to showcase your work.
+
+
+
+## How to use
+1. Clone/Download the repo.
+2. Run  ``` npm install ```.
+3. Change the values in ```src/resumeData.js``` to suit your use-case.
+4. Run ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
+5. Make required changes in ```src/resumeData.js``` to suit your needs.
+
---
+++ src/resumeData.js
@@ -0,0 +1,111 @@
+let resumeData = {
+    "imagebaseurl":"https://tinakaranovic7.github.io/UpdatedPortfolio/",
+    "name": "Tina Karanovic",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to learn about new technologies in my free time and help people resolve technical problems.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/tinakaranovic7",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://stackoverflow.com/users/12289127/tina-karanovic",
+          "className":"fa fa-stack-overflow"
+        }
+      ],
+    "aboutme":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+    "address":"Virginia, USA",
+    "website":"https://tinakaranovic7.github.io/UpdatedPortfolio/",
+    "education":[
+      {
+        "UniversityName":"GW University",
+        // "specialization":"Some specialization",
+         "MonthOfPassing":"April",
+         "YearOfPassing":"2020",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Faculty of Economics, Finance and Administration",
+        // "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2016",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    // "work":[
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   },
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+    // ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"TOPIKS app",
+        "imgurl":"images/topiks.jpg",
+        "url":"https://zaydorade.github.io/Project-1/",
+      },
+      {
+        "name":"project2",
+        "description":"Spa Relax",
+        "imgurl":"images/spaRelax.jpg",
+        "url":"https://project2-spa.herokuapp.com/",
+      },
+      {
+        "name":"project3",
+        "description":"Budget app",  
+        "imgurl":"images/budget-tracker.jpg",
+        "url":"https://offline-budget-application.herokuapp.com/",
+
+      },
+      {
+        "name":"project4",
+        "description":"Weather",
+        "imgurl":"images/weatherDashboard.jpg",
+        "url":"https://tinakaranovic7.github.io/WeatherDashboard/",
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     // "description":"This is a sample testimonial",
+    //     // "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
